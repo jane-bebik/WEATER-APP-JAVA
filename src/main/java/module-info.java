c1b21e91baa39c2efd2264d4eceb21d22b08a197
Jane bebik <118847459+jane-bebik@users.noreply.github.com>
2024-10-14 19:27:49 -0400

    THAT IS A NEW START

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.weatherappjane {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.weatherappjane to javafx.fxml;
+    exports com.example.weatherappjane;
+}
