2c46e18236f4d12d3b5635a8d9627dbe349abfb6
Jane bebik <118847459+jane-bebik@users.noreply.github.com>
2024-10-14 23:39:44 -0400

    added icon

--- src/main/java/com/example/weatherappjane/WeatherAppGUI.java
+++ src/main/java/com/example/weatherappjane/WeatherAppGUI.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class WeatherAppGUI extends Application {
@@ -12,7 +13,10 @@ public class WeatherAppGUI extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/com/example/weatherappjane/WeatherAppGUI.fxml"));
 
         Scene scene = new Scene(root, 500, 400);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());  // Link CSS file
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        // Set the icon for the application
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/weatherappjane/app-icon.png")));
 
         primaryStage.setTitle("Jane's Weather App");
         primaryStage.setScene(scene);
